cb44664406c8b4d7b47905e77bf182c401fa0980
ralei0 <56300491+ralei0@users.noreply.github.com>
2023-05-09 05:36:32 -0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'Enter your Email Id here.'
-# EMAIL_HOST_PASSWORD = 'Here you need to enter your google apps passwords. If do not know then follow this process below'
+EMAIL_HOST_USER = 'Enter your Email Id here.'
+EMAIL_HOST_PASSWORD = 'Here you need to enter your google apps passwords. If do not know then follow this process below'
 
 """
 Steps to get password for Email host Password
